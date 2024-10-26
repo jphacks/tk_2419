6e9ed0ef553400d6a92da0c27adac18b03b92f86
0yaizu <kaira.oyaizu@gmail.com>
2024-10-26 14:23:52 +0900

    完成

--- server.js
+++ server.js
@@ -6,19 +6,27 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
+const folderPath = path.join(__dirname, 'modules');
+const moduleFiles = fs.readdirSync(folderPath);
+
+for (const file of moduleFiles) {
+	const filePath = path.join(folderPath, file);
+	require(filePath);
+}
+
 // get host ip
 function getIpAddress() {
-  const nets = os.networkInterfaces();
-  const net = nets["en0"]?.find((v) => v.family == "IPv4");
-  return !!net ? net.address : null;
+	const nets = os.networkInterfaces();
+	const net = nets["en0"]?.find((v) => v.family == "IPv4");
+	return !!net ? net.address : null;
 }
 const hostname = getIpAddress();
-app.use('/public',express.static(path.join(__dirname,'static')));
-app.use(bodyParser.urlencoded({extended:false}));
+app.use('/public', express.static(path.join(__dirname, 'static')));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // index.htmlを返す
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'index.html'));
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // 3000番ポートで待ち受け
