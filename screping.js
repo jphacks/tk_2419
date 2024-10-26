080408d18b0f9213318fab4c149e7c89b08d7417
YzkSK <s2422014@stu.musashino-u.ac.jp>
2024-10-27 08:25:03 +0900

    コードの変更

--- screping.js
+++ screping.js
@@ -1,4 +1,6 @@
 const playwright = require('playwright');
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('movies.db');
 
 async function getMovieTitles(url) {
     const browser = await playwright.chromium.launch();
@@ -19,7 +21,37 @@ async function getMovieTitles(url) {
     return movieTitles;
 }
 
-const url = 'https://hlo.tohotheater.jp/net/schedule/084/TNPI2000J01.do';
-getMovieTitles(url).then((movieTitles) => {
+async function getCinemasName(url) {
+    const browser = await playwright.chromium.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await page.goto(url);
+    const arrayOfLocator = page.locator('div[class="theater-list"]').locator('ul').locator('span');
+    const count = await arrayOfLocator.count();
+    let cinemas = [];
+
+    for (var index = 0; index < count; index++) {
+        const element = await arrayOfLocator.nth(index);
+        const movieTitle = await element.innerText();
+        cinemas.push(movieTitle);
+    }
+    await browser.close();
+    return cinemas;
+}
+
+const cinemasUrl = 'https://www.tohotheater.jp/theater/find.html';
+getCinemasName(cinemasUrl).then((cinemasName) => {
+    // 一個飛ばしでリストに入れ、日本語の映画館名だけを取得し、後半の英語の映画館名は削除
+    let cinemas = [];
+    for (var index = 0; index < cinemasName.length; index += 2) {
+        cinemas.push(cinemasName[index]);
+    }
+    console.log(cinemas);
+
+
+const movieUrl = 'https://hlo.tohotheater.jp/net/schedule/084/TNPI2000J01.do';
+getMovieTitles(movieUrl).then((movieTitles) => {
     console.log(movieTitles);
+    });
 });
