370e272ac3d988c97c3d422c92c8e0cbc99a00f6
0yaizu <kaira.oyaizu@gmail.com>
2024-10-26 23:25:57 +0900

    add framer-motion

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import SearchBox from "./SearchBox.jsx";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+			<SearchBox />
     </div>
   );
 }
