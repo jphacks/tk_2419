28a344bb900e1ac1122e2426d7c14c13de28bb2b
0yaizu <kaira.oyaizu@gmail.com>
2024-10-27 14:53:03 +0900

    一旦変数に最低限のマーカーを示した。リストに格納された情報をマーカーに記述。

--- src/client/DisplayGoogleMap.jsx
+++ src/client/DisplayGoogleMap.jsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
 import configs from "./assets/config.json";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
 import { useGeolocated } from "react-geolocated";
 
 // get google api key
 const googleApiKey = configs.google_api_key;
 
 export const DisplayGoogleMap = (props) => {
+	const [pinList, setPinList] = useState([
+		{
+			name: "TOHOシネマズ 市原TOHO CINEMAS ICHIHARA",
+			location: {
+				lat: 35.5079342,
+				lng: 140.1007624
+			}
+		},
+		{
+			name: "TOHOシネマズ 上野TOHO CINEMAS TACHIKAWA UENO",
+			location: {
+				lat: 35.7068072,
+				lng: 139.7731154
+			}
+		}
+	]);
 	const { coords, isGeolocationAvailable, isGeolocationEnabled } = useGeolocated({
 		positionOptions: {
 			enableHighAccuracy: false,
@@ -30,16 +46,23 @@ export const DisplayGoogleMap = (props) => {
 			<div>Geolocation is not enabled</div>
 		) : coords ? (
 			<>
-				<p>Search Key Word: {props.keyword}</p>
-				<p class="latitude">{coords.latitude}</p>
-				<p class="longitude">{coords.longitude}</p>
+				{/* <p>Search Key Word: {props.keyword}</p> */}
+				{/* <p class="latitude">{coords.latitude}</p> */}
+				{/* <p class="longitude">{coords.longitude}</p> */}
 				<div id="info">
 					<LoadScript googleMapsApiKey={googleApiKey}>
 						<GoogleMap
 							mapContainerStyle={containerStyle}
 							center={center}
-							zoom={17}
-						/>
+							zoom={17}>
+							{
+								pinList.map(item => {
+									return (
+										<MarkerF key={item.name} position={item.location} />
+									)
+								})
+							}
+						</GoogleMap>
 					</LoadScript>
 				</div>
 			</>
--- src/server/main.js
+++ src/server/main.js
@@ -1,11 +1,16 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import sqlite3 from "sqlite3";
 
 const app = express();
 
 app.get("/hello", (req, res) => {
   res.send("Hello Vite + React!");
 });
+
+app.get("/getdb", (req, res) => {
+	
+})
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
